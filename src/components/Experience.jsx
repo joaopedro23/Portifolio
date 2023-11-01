e86d278cd103e28e6a90ecb178300835e2a73d5e
joaopedro23 <joaopedro105manairapb@gmail.com>
2023-11-01 17:00:29 -0300

    2 commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -64,7 +64,7 @@ const Experience = () => {
           What I have done so far
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
-      experiencia de ajuda 
+      experiencia em Projetos Pessoais
         </h2>
       </motion.div>
 
